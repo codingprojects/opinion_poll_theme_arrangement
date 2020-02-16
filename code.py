1d23b5e64904e00004b3ba02f7f15448d993f3c2
codingprojects <61053623+codingprojects@users.noreply.github.com>
2020-02-16 12:01:13 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,26 @@
-if chosen_arrangement_first_three_themes= economy, environment, energy, transport, immigration, safety, health care, education, science, culture, infrastructure, agriculture
+if chosen_arrangement_first_three_themes= economy, environment, energy
 then selected_party=partyA
 
-if chosen_arrangement_first_three_themes= economy, energy, transport, immigration, safety, health care, education, science, culture, infrastructure, agriculture
-then selected_party=partyA 
+if chosen_arrangement_first_three_themes= environment, energy, transport,
+then selected_party=partyB
+
+if chosen_arrangement_first_three_themes= economy, environment, agriculture
+then selected_party=partyC
+
+if chosen_arrangement_first_three_themes= immigration, safety, health care
+then selected_party=partyD
+
+if chosen_arrangement_first_three_themes= education, science, culture,
+then selected_party=partyE
+
+if chosen_arrangement_first_three_themes=immigration, economy, infrastructure
+then selected_party=partyF
+
+# Give parties a label with a local party name for displaying purposes
+partyA= set_local_party_name_here
+partyB= set_local_party_name_here
+partyC= set_local_party_name_here
+partyD= set_local_party_name_here
+partyE= set_local_party_name_here
+partyF= set_local_party_name_here
+partyG= set_local_party_name_here
