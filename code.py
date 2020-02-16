8ddfc962756e7fc9900f7e7b31b4d0869c2a25dd
codingprojects <61053623+codingprojects@users.noreply.github.com>
2020-02-16 12:44:20 +0100

    Update code.py

--- code.py
+++ code.py
@@ -46,37 +46,37 @@ Then display "You have chosen for [display label:partyG]"
 
 # Now start the questions on the chosen themes for increased opinion precision
 If chosen_arrangement_first_three_themes includes economy
-then launch q10r_economyquestions
+then launch q10r_economy_questions
 
 If chosen_arrangement_first_three_themes includes environment
-then launch q10r_environmentquestions
+then launch q10r_environment_questions
 
 If chosen_arrangement_first_three_themes includes energy
-then launch q10r_energyquestions
+then launch q10r_energy_questions
 
 If chosen_arrangement_first_three_themes includes transport
-then launch q10r_transportquestions
+then launch q10r_transport_questions
 
 If chosen_arrangement_first_three_themes includes immigration
-then launch q10r_immigrationquestions
+then launch q10r_immigration_questions
 
 If chosen_arrangement_first_three_themes includes safety
-then launch q10r_safetyquestions
+then launch q10r_safety_questions
 
 If chosen_arrangement_first_three_themes includes health care
-then launch q10r_healthcarequestions
+then launch q10r_healthcare_questions
 
 If chosen_arrangement_first_three_themes includes education
-then launch q10r_educationquestions
+then launch q10r_education_questions
 
 If chosen_arrangement_first_three_themes includes science
-then launch q10r_sciencequestions
+then launch q10r_science_questions
 
 If chosen_arrangement_first_three_themes includes culture
-then launch q10r_culturequestions
+then launch q10r_culture_questions
 
 If chosen_arrangement_first_three_themes includes infrastructure
-then launch q10r_culturequestions
+then launch q10r_infrastructure_questions
 
 If chosen_arrangement_first_three_themes includes agriculture
-then launch q10r_culturequestions
+then launch q10r_agriculture_questions
