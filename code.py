23a673ce375b2591912ebda079acdee3726b3429
codingprojects <61053623+codingprojects@users.noreply.github.com>
2020-02-16 11:27:26 +0100

    Create code.py

---
+++ code.py
@@ -0,0 +1,5 @@
+if chosen_arrangement_first_three_themes= economy, environment, energy, transport, immigration, safety, health care, education, science, culture, infrastructure, agriculture
+then selected_party=partyA
+
+if chosen_arrangement_first_three_themes= economy, energy, transport, immigration, safety, health care, education, science, culture, infrastructure, agriculture
+then selected_party=partyA 
